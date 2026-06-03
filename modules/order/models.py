6022edbad3501551ef013233e827dd46a9c877a5
DuongLH <gacon1092@gmail.com>
2026-06-03 15:43:12 +0700

    update order

--- modules/order/models.py
+++ modules/order/models.py
@@ -100,6 +100,10 @@ class OrdersModel(SoftDeleteMixin, Base):
     carrier_code: Mapped[str] = mapped_column(String(20))
     carrier_tracking_code: Mapped[str] = mapped_column(String(50))
     is_rewarded: Mapped[int] = mapped_column(Integer)
+    sub_status: Mapped[int] = mapped_column(Integer)
+    note: Mapped[str] = mapped_column(Text)
+    source_id: Mapped[int] = mapped_column(Integer)
+    source_note: Mapped[str] = mapped_column(String(200))
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
     
